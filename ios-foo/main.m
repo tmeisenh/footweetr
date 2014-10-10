5079bbd4783339a1a881191f85854010846c9438
Travis Meisenheimer <travis.meisenheimer@asynchrony.com>
2014-10-10 18:55:18 -0500

    initial creation

---
+++ ios-foo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios-foo
+//
+//  Created by dev1 on 10/10/14.
+//  Copyright (c) 2014 asynchrony.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FOOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FOOAppDelegate class]));
+    }
+}
---
+++ ios-fooTests/ios_fooTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_fooTests.m
+//  ios-fooTests
+//
+//  Created by dev1 on 10/10/14.
+//  Copyright (c) 2014 asynchrony.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_fooTests : XCTestCase
+
+@end
+
+@implementation ios_fooTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
