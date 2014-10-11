7fbeede75df052d5d4a8f54b58b28f46ed9c0992
Travis Meisenheimer <travis.meisenheimer@asynchrony.com>
2014-10-10 20:32:52 -0500

    added empty test

---
+++ ios-foo/test/FOONSDocumentsDirectoryLocatorTests.m
@@ -0,0 +1,15 @@
+#import <XCTest/XCTest.h>
+
+@interface FOONSDocumentsDirectoryLocatorTests : XCTestCase
+
+@end
+
+@implementation FOONSDocumentsDirectoryLocatorTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+
+
+@end
