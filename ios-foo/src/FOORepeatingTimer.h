3eefb8a36aeb4fdee171c386d8052bcc14ce69ac
Travis Meisenheimer <travis.meisenheimer@asynchrony.com>
2014-11-14 22:13:25 -0600

    Added testable timer.

---
+++ ios-foo/src/FOORepeatingTimer.h
@@ -0,0 +1,13 @@
+//
+//  FOORepeatingTimer.h
+//  ios-foo
+//
+//  Created by Travis Meisenheimer on 11/14/14.
+//  Copyright (c) 2014 asynchrony.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FOORepeatingTimer : NSObject
+
+@end
---
+++ ios-foo/src/FOORepeatingTimer.m
@@ -0,0 +1,13 @@
+//
+//  FOORepeatingTimer.m
+//  ios-foo
+//
+//  Created by Travis Meisenheimer on 11/14/14.
+//  Copyright (c) 2014 asynchrony.com. All rights reserved.
+//
+
+#import "FOORepeatingTimer.h"
+
+@implementation FOORepeatingTimer
+
+@end
