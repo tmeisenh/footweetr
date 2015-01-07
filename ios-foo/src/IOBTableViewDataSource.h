4609ee1c030822f6f22556a179fe8b7b6a1b0202
Travis Meisenheimer <travis.meisenheimer@asynchrony.com>
2015-01-06 19:54:49 -0600

    tweaked data source api

--- ios-foo/src/IOBTableViewDataSource.h
+++ ios-foo/src/IOBTableViewDataSource.h
@@ -1,13 +1,27 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+@protocol IOBTableViewDataSourceDelegate <NSObject>
+
+@optional
+- (void)removeSection:(NSInteger)section;
+- (void)addSection:(NSInteger)section;
+
+- (void)removeRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)reloadRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)insertRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)moveRowFromOldIndexPath:(NSIndexPath *)oldIndexPath toNewIndexPath:(NSIndexPath *)newIndexPath;
+
+@end
+
 @protocol IOBTableViewDataSource <NSObject>
 
+@required
 - (NSInteger)numberOfSections;
 - (NSInteger)numberOfRowsInSection:(NSInteger)section;
-- (NSObject *)objectAtSection:(NSInteger)section;
 - (NSObject *)objectAtIndexPath:(NSIndexPath *)indexPath;
 
-- (void)removeObjectAtIndexPath:(NSIndexPath *)indexPath;
+@optional
+- (NSObject *)objectAtSection:(NSInteger)section;
 
 @end
